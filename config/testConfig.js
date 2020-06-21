a455afb7f06295d8e0497c4291892d1cc45489ec
BrianHHough <brianhoughmedia@gmail.com>
2020-06-21 09:15:19 -0400

    updating testConfig.js

--- config/testConfig.js
+++ config/testConfig.js
@@ -20,17 +20,22 @@ var Config = async function(accounts) {
     ];
 
 
-    let owner = accounts[0];
-    let firstAirline = accounts[1];
+    let owner = accounts[1];
+    let firstAirline = accounts[0];
 
     let flightSuretyData = await FlightSuretyData.new();
-    let flightSuretyApp = await FlightSuretyApp.new();
+    
+    let flightSuretyApp = await FlightSuretyApp.new(FlightSuretyData.address);
+    // ORIGINAL
+    // let flightSuretyApp = await FlightSuretyApp.new();
 
     
     return {
         owner: owner,
         firstAirline: firstAirline,
         weiMultiple: (new BigNumber(10)).pow(18),
+        // testAddresses: accounts,
+        // ORIGINAL
         testAddresses: testAddresses,
         flightSuretyData: flightSuretyData,
         flightSuretyApp: flightSuretyApp
